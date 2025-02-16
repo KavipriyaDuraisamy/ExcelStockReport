19b2f394a64fdfa005ada31637161d0508935a5c
KavipriyaDuraisamy <kavipryaji.duraisamy@gmail.com>
2025-02-17 00:29:34 +0530

    implement the routing changes and default path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExcelUploaderComponent } from './excel-uploader/excel-uploader.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"exceldata",
+    component:ExcelUploaderComponent
+  },
+  { path: '', redirectTo: 'exceldata', pathMatch: 'full' },
+  { path: '**', redirectTo: 'exceldata', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,18 @@
-var URLHOST = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
 
-  //Server URL
-  SECRETKEY: 'RADIANTWORKFLOW',
-  // ViewerURL: URLHOST+"/FreedomNeoApp/#/",
-  // WebUploadURL: URLHOST+"/FreedomWebUploaderApp/#/",
-  // APIURL: URLHOST+ "/Radiantworkflowapi/api/v1/",
-
-  ViewerURL: URLHOST + "/FreedomNeoApp/#/",
-  WebUploadURL: URLHOST +"/FreedomWebUploaderApp/#/",
-  APIURL: URLHOST +"/Radiantworkflowapi/api/v1/",
-
-  //Local URL
-  // ViewerURL: "http://192.168.1.112:4200/#/",
-  // WebUploadURL : "http://192.168.1.105:4200/#/",
-  // APIURL: "http://192.168.1.112:7006/",
+  APIURL: "http://localhost:5277/api/StockData/",
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
